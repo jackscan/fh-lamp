3246af40cd4c34b0d7dcf73c50b3ef9393ebb28e
Mathias Fiedler <mathias.fiedler@gmail.com>
2018-08-22 19:44:53 +0200

    increase resolution of gamma table and do some dithering
    Gamma range is now [0, 255 * 4].

--- Pixel.c
+++ Pixel.c
@@ -21,35 +21,25 @@ static struct
 	uint8_t luminance;
 } pixel;
 
-// const __flash uint8_t GammaTable[20] = {
-// 	0,  1,  2,  3,  4,   8,   13,  19,  27,  37,
-// 	48, 62, 77, 95, 115, 138, 163, 191, 222, 255,
-// };
-
-// const __flash uint8_t GammaTable[NUM_LUMINANCE_STEPS] = {
-//     0,   1,   5,  15,  31,  55,  89, 133, 188, 255
-// };
-
-const __flash uint8_t GammaTable[NUM_LUMINANCE_STEPS] = {
-	0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-	0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   1,   1,   1,   1,   1,
-	1,   1,   1,   1,   1,   1,   2,   2,   2,   2,   2,   2,   2,   2,   3,
-	3,   3,   3,   3,   3,   4,   4,   4,   4,   5,   5,   5,   5,   5,   6,
-	6,   6,   6,   7,   7,   7,   8,   8,   8,   9,   9,   9,   10,  10,  10,
-	11,  11,  11,  12,  12,  13,  13,  13,  14,  14,  15,  15,  16,  16,  17,
-	17,  18,  18,  19,  19,  20,  20,  21,  21,  22,  22,  23,  24,  24,  25,
-	25,  26,  27,  27,  28,  29,  29,  30,  31,  31,  32,  33,  34,  34,  35,
-	36,  37,  38,  38,  39,  40,  41,  42,  42,  43,  44,  45,  46,  47,  48,
-	49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
-	64,  65,  66,  68,  69,  70,  71,  72,  73,  75,  76,  77,  78,  80,  81,
-	82,  84,  85,  86,  88,  89,  90,  92,  93,  94,  96,  97,  99,  100, 102,
-	103, 105, 106, 108, 109, 111, 112, 114, 115, 117, 119, 120, 122, 124, 125,
-	127, 129, 130, 132, 134, 136, 137, 139, 141, 143, 145, 146, 148, 150, 152,
-	154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182,
-	184, 186, 188, 191, 193, 195, 197, 199, 202, 204, 206, 209, 211, 213, 215,
-	218, 220, 223, 225, 227, 230, 232, 235, 237, 240, 242, 245, 247, 250, 252,
-	255,
-};
+const __flash uint16_t GammaTable[256] = {
+	0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,
+	1,   1,   1,   1,   1,   1,   2,   2,   2,   2,   2,   3,   3,   3,   4,
+	4,   4,   5,   5,   5,   6,   6,   7,   7,   8,   8,   9,   9,   10,  11,
+	11,  12,  13,  13,  14,  15,  16,  16,  17,  18,  19,  20,  21,  22,  23,
+	24,  25,  26,  27,  28,  29,  30,  32,  33,  34,  35,  37,  38,  39,  41,
+	42,  44,  45,  47,  48,  50,  52,  53,  55,  57,  59,  60,  62,  64,  66,
+	68,  70,  72,  74,  76,  78,  80,  83,  85,  87,  89,  92,  94,  97,  99,
+	102, 104, 107, 109, 112, 115, 117, 120, 123, 126, 129, 132, 135, 138, 141,
+	144, 147, 150, 153, 156, 160, 163, 167, 170, 173, 177, 181, 184, 188, 191,
+	195, 199, 203, 207, 211, 215, 219, 223, 227, 231, 235, 239, 244, 248, 252,
+	257, 261, 266, 270, 275, 280, 284, 289, 294, 299, 304, 309, 314, 319, 324,
+	329, 334, 339, 345, 350, 355, 361, 366, 372, 378, 383, 389, 395, 401, 406,
+	412, 418, 424, 430, 437, 443, 449, 455, 462, 468, 475, 481, 488, 494, 501,
+	508, 515, 521, 528, 535, 542, 549, 557, 564, 571, 578, 586, 593, 601, 608,
+	616, 623, 631, 639, 647, 655, 662, 670, 679, 687, 695, 703, 711, 720, 728,
+	737, 745, 754, 762, 771, 780, 789, 798, 807, 816, 825, 834, 843, 853, 862,
+	871, 881, 890, 900, 910, 919, 929, 939, 949, 959, 969, 979, 989, 999, 1010,
+	1020};
 
 void Pixel_Setup(void)
 {
@@ -138,19 +128,28 @@ static void writePixels(void)
 	_delay_us(80);
 }
 
-static inline uint8_t minu8(uint8_t a, uint8_t b)
+static uint8_t ditheredGammaLookup(uint8_t i, uint8_t c)
 {
-	return a < b ? a : b;
+	uint16_t g = GammaTable[c];
+	uint8_t a = (uint8_t)(g >> 2);
+	uint8_t b = (uint8_t)(g & 0x3);
+	if ((i & 0x3) < b) ++a;
+	return a;
 }
 
 static inline void setPixel(uint8_t i, uint8_t r, uint8_t g, uint8_t b, uint8_t w)
 {
-	pixel.data[i * BYTES_PER_PIXEL + 0] = g;
-	pixel.data[i * BYTES_PER_PIXEL + 1] = r;
-	pixel.data[i * BYTES_PER_PIXEL + 2] = b;
+	pixel.data[i * BYTES_PER_PIXEL + 0] = ditheredGammaLookup(i, g);
+	pixel.data[i * BYTES_PER_PIXEL + 1] = ditheredGammaLookup(i, r);
+	pixel.data[i * BYTES_PER_PIXEL + 2] = ditheredGammaLookup(i, b);
 #if BYTES_PER_PIXEL > 3
-	pixel.data[i * BYTES_PER_PIXEL + 3] = w;
+	pixel.data[i * BYTES_PER_PIXEL + 3] = ditheredGammaLookup(i, w);
 #endif
+}
+
+static inline uint8_t minu8(uint8_t a, uint8_t b)
+{
+	return a < b ? a : b;
 }
 
 static void setAllPixel(uint8_t r, uint8_t g, uint8_t b)
@@ -189,17 +188,6 @@ void Pixel_SetPower(bool on)
 	}
 }
 
-
-
-static uint8_t gammaLookup(uint8_t c)
-{
-	return GammaTable[c];
-}
-
-
-
-
-
 void Pixel_Clear(void)
 {
     setAllPixel(0, 0, 0);
@@ -207,7 +195,7 @@ void Pixel_Clear(void)
 
 void Pixel_SetAll(uint8_t r, uint8_t g, uint8_t b)
 {
-    setAllPixel(gammaLookup(r), gammaLookup(g), gammaLookup(b));
+    setAllPixel(r, g, b);
 }
 
 void Pixel_Set(int8_t index, uint8_t r, uint8_t g, uint8_t b)
@@ -223,8 +211,7 @@ void Pixel_Set(int8_t index, uint8_t r, uint8_t g, uint8_t b)
 #else
 	uint8_t w = 0;
 #endif
-	setPixel(index, gammaLookup(r), gammaLookup(g), gammaLookup(b),
-			 gammaLookup(w));
+	setPixel(index, r, g, b, w);
 }
 
 void Pixel_Write(void)
