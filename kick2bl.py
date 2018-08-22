24c7bdc64196f3128522209ee464af0df91d6f82
Mathias Fiedler <mathias.fiedler@gmail.com>
2018-08-22 20:01:45 +0200

    cleanup kick2bl.py

--- kick2bl.py
+++ kick2bl.py
@@ -1,16 +1,4 @@
 #!/usr/bin/env python
-# import serial, sys
-# serialPort = sys.argv[1]
-# ser = serial.Serial(
-#     port=serialPort,
-#     baudrate=1200,
-#     parity=serial.PARITY_NONE,
-#     stopbits=serial.STOPBITS_ONE,
-#     bytesize=serial.EIGHTBITS
-# )
-# ser.isOpen()
-# ser.close() # always close port
-
 import argparse
 import serial
 import os.path
@@ -20,8 +8,6 @@ parser = argparse.ArgumentParser(description='Reset an Arduino')
 parser.add_argument('port', nargs=1, help='Serial device e.g. /dev/ttyACM0')
 args = parser.parse_args()
 
-
-# ser.setBaudrate (1200)
 ser = serial.Serial(args.port[0], 1200)
 
 ser.setRTS(True)  # RTS line needs to be held high and DTR low
